0048295f70cbaad52f5b69b1f4d01f6f8721baee
Apollo Akora <apolloakora@gmail.com>
2019-07-21 21:40:22 +0100

    ensured that the time returned was suitable for GMT readers.

--- resource.tags-main.tf
+++ resource.tags-main.tf
@@ -14,23 +14,25 @@ data aws_region with {}
 locals
 {
     /*
-     | Example Note ~> "was created by eks-curator in London at 14:35."
+     | Example Note ~> "was created in Ireland on Tue, 02 Jan 2018 23:12:01."
      */
     username   = "${ element( split( "/", data.aws_caller_identity.with.arn ), 1 ) }"
     city_plus  = "${ element( split( "(", data.aws_region.with.description ), 1 ) }"
     city_name  = "${ element( split( ")", local.city_plus ), 0 ) }"
-    time_hhmm  = "${ substr( timestamp(), 11, 5 ) }"
-    descriptor = "was created by ${local.username} in ${local.city_name} at ${local.time_hhmm}."
+
+    time_value = timeadd( timestamp(), "1h" )
+    t_readable = formatdate( "EEE, DD MMM YYYY hh:mm:ss", local.time_value )
+    descriptor = "was created in ${ local.city_name } on ${ local.t_readable }."
 
     /*
      | Raw Timestamp = 2018-09-11T14:35:59Z
-     | Eco Timestamp = "80911-1435"
+     | Eco Timestamp = "809111435"
      */
-    year_y    = "${ substr( element( split( "-", timestamp() ), 0 ), 3, 1 ) }"
-    month_no  = "${ element( split( "-", timestamp() ), 1 ) }"
-    day_dd    = "${ substr( element( split( "-", timestamp() ), 2 ), 0, 2 ) }"
-    hour_hh   = "${ substr( element( split( ":", timestamp() ), 0 ), 11, 2 ) }"
-    min_mm    = "${ element( split( ":", timestamp() ), 1 ) }"
+    year_y    = "${ substr( element( split( "-", local.time_value ), 0 ), 3, 1 ) }"
+    month_no  = "${ element( split( "-", local.time_value ), 1 ) }"
+    day_dd    = "${ substr( element( split( "-", local.time_value ), 2 ), 0, 2 ) }"
+    hour_hh   = "${ substr( element( split( ":", local.time_value ), 0 ), 11, 2 ) }"
+    min_mm    = "${ element( split( ":", local.time_value ), 1 ) }"
     timestamp = "${ local.year_y }${ local.month_no }${ local.day_dd }${ local.hour_hh }${ local.min_mm }"
 }
 
