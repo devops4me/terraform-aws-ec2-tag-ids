ba48363c2548db305c71fc8541b6aae03ab00eb8
Apollo Akora <apolloakora@gmail.com>
2019-07-23 10:20:45 +0100

    removing commas from the description field for some AWS components like ALB

--- resource.tags-main.tf
+++ resource.tags-main.tf
@@ -14,25 +14,25 @@ data aws_region with {}
 locals {
 
     /*
-     | Example Note ~> "was created in Ireland on Tue, 02 Jan 2018 23:12:01."
+     | Example Note ~> "was created in Ireland on Tue 02 Jan 2018 at 23:12:01."
      */
-    username   = "${ element( split( "/", data.aws_caller_identity.with.arn ), 1 ) }"
-    city_plus  = "${ element( split( "(", data.aws_region.with.description ), 1 ) }"
-    city_name  = "${ element( split( ")", local.city_plus ), 0 ) }"
+    username   = element( split( "/", data.aws_caller_identity.with.arn ), 1 )
+    city_plus  = element( split( "(", data.aws_region.with.description ), 1 )
+    city_name  = element( split( ")", local.city_plus ), 0 )
 
-    time_value = "${ timeadd( timestamp(), "1h" ) }"
-    t_readable = "${ formatdate( "EEE, DD MMM YYYY hh:mm:ss", local.time_value ) }"
+    time_value = timeadd( timestamp(), "1h" )
+    t_readable = replace( formatdate( "EEE, DD MMM YYYY hh:mm:ss", local.time_value ), ",", "" )
     descriptor = "was created in ${ local.city_name } on ${ local.t_readable }."
 
     /*
      | Raw Timestamp = 2018-09-11T14:35:59Z
      | Eco Timestamp = "809111435"
      */
-    year_y    = "${ substr( element( split( "-", local.time_value ), 0 ), 3, 1 ) }"
-    month_no  = "${ element( split( "-", local.time_value ), 1 ) }"
-    day_dd    = "${ substr( element( split( "-", local.time_value ), 2 ), 0, 2 ) }"
-    hour_hh   = "${ substr( element( split( ":", local.time_value ), 0 ), 11, 2 ) }"
-    min_mm    = "${ element( split( ":", local.time_value ), 1 ) }"
-    timestamp = "${ local.year_y }${ local.month_no }${ local.day_dd }${ local.hour_hh }${ local.min_mm }"
+    year_y    = substr( element( split( "-", local.time_value ), 0 ), 3, 1 )
+    month_no  = element( split( "-", local.time_value ), 1 )
+    day_dd    = substr( element( split( "-", local.time_value ), 2 ), 0, 2 )
+    hour_hh   = substr( element( split( ":", local.time_value ), 0 ), 11, 2 )
+    min_mm    = element( split( ":", local.time_value ), 1 )
+    timestamp = local.year_y }${ local.month_no }${ local.day_dd }${ local.hour_hh }${ local.min_mm
 }
 
